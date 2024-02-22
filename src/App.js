298993c225d9d4a460e9387c13e48835e7214e9a
joshi-durgesh <durgesh7joshi9@gmail.com>
2024-02-23 00:51:56 +0530

    router dom setup and links imported

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import Body from "./components/Body";
+import { Outlet, createBrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
+import SignIn from "./components/SignIn";
+
 function App() {
   return (
-    <div className='App'>
-      <h1 className='text-2xl text-red-200'>Hello world</h1>
+    <div>
+      <Header />
+      <Outlet />
     </div>
   );
 }
+
+export const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <SignIn />,
+  },
+]);
 
 export default App;
